2fffcaee543cd665fda5537a14414698b3b0e24b
Lucas Ernesto Kindinger <kindingerlek@hotmail.com>
2021-09-23 11:13:40 -0300

    feat: create new @requireProcessSuccess decorator

--- processChain/requireProcess.ts
+++ processChain/requireProcess.ts
@@ -1,5 +1,5 @@
 import { PipelineData } from "./pipeline";
-import Process from "./process";
+import Process, { ProcessStatus } from "./process";
 
 export function requireProcess(...previousOperations: (new () => Process)[]) {
   return (target: any, key: string, propDesc: PropertyDescriptor) => {
@@ -13,6 +13,28 @@ export function requireProcess(...previousOperations: (new () => Process)[]) {
 
         if (pipelineData[op.name] == undefined)
           throw new Error(`The result of '${op.name}' is required but was not found on pipeline`);
+      }
+
+      return originalMethod(pipelineData);
+    };
+  };
+}
+
+export function requireProcessSuccess(...previousOperations: (new () => Process)[]) {
+  return (target: any, key: string, propDesc: PropertyDescriptor) => {
+    const originalMethod = propDesc.value!;
+
+
+    propDesc.value = (pipelineData: PipelineData<any>) => {
+      for (let op of previousOperations) {
+        if (op.name == target.constructor.name)
+          throw new Error(`The process ${target.constructor.name} can't require it's self.`);
+
+        if (pipelineData[op.name] == undefined)
+          throw new Error(`The result of '${op.name}' is required but was not found on pipeline`);
+
+        if(pipelineData[op.name].status != ProcessStatus.done)
+          throw new Error(`The process '${op.name}' has not a success status`);
       }
 
       return originalMethod(pipelineData);
